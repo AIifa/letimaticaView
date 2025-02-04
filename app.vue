2ceeb530d6821f4b452b9b18e653d0d7f0f9a8fe
Andrey <alifa.stdn@gmail.com>
2025-02-04 21:49:39 +0300

    Добавление обработки файла

--- app.vue
+++ app.vue
@@ -2,5 +2,11 @@
   <div>
     <NuxtRouteAnnouncer />
     <NuxtWelcome />
+    {{ data.value }}
   </div>
 </template>
+
+<script setup>
+const { data } = await useFetch('/api/read')
+console.log(data.value.value)
+</script>
