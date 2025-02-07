4aec5fee0664c86a79cb2fc2e5ac0843bfcccddb
Andrey <alifa.stdn@gmail.com>
2025-02-07 23:58:56 +0300

    доработки дизайна и запросов

--- pages/index.vue
+++ pages/index.vue
@@ -1,12 +1,20 @@
 <template>
   <div>
-    <LoaderItem />
-    {{ data }}
-    <div 
+    <UInput type="file" size="sm" icon="i-heroicons-folder" />
+
+    <span> {{ schematicName }} </span>
+    <LoaderItem
+      :list="arrayCount"
+    />
+    <div
+      v-if="listImage"
       v-for="(name, i) in arrayPallete"
     >
       <MineItem
         :name="name"
+        :image="listImage[i]"
+        :current="arrayCount[i]"
+        :total="arrayCount[i]"
       />
     </div>
   </div>
@@ -16,21 +24,30 @@
 const { data } = await useFetch('/api/read');
 
 const schematic = data.value.value;
-const regionNames = Object.keys(schematic.Regions.value)
+const schematicName = data.value.value.Metadata.value.Name.value;
+console.log(data.value.value)
+const regionNames = Object.keys(schematic.Regions.value);
 
 const arrayPallete = [
   ...new Set(
     regionNames
       .flatMap(regionName => schematic.Regions.value[regionName]?.value.BlockStatePalette?.value.value)
-      .map(item => item.Name.value)
-      .filter(name => name !== 'minecraft:air')
-  )
-,
-'minecraft:blue_orchid',
-'minecraft:brown_mushroom'
+      .map(item => item.Name.value.slice(10))
+      .filter(name => name !== 'air')
+  ),
+  'blue_orchid',
+  'brown_mushroom',
 ];
+const arrayCount = ref(new Array(arrayPallete.length).map((item) => item = { current: 0, total: 0 }));
 
-console.log(arrayPallete);
+let listImage = reactive({});
+useFetch('/api/mc', {
+    method: 'PUT',
+    body: { listName: arrayPallete },
+  })
+  .then((res) => {
+    listImage = res.data;
+  });
 </script>
 
 <style scoped>
