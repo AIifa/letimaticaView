1e29518782c83713f1c19214b96c3f8064e48899
LazySelkie <54956262+LazySelkie@users.noreply.github.com>
2025-02-13 01:17:17 +0300

    добавлена часть недостающих текстур

--- server/api/processFile.put.js
+++ server/api/processFile.put.js
@@ -77,6 +77,24 @@ export default defineEventHandler(async (event) => {
         return result;
     }, {})
     console.log("materialList: ", materialList)
+
+    // Чистка всяких дублирующихся предметов (например, что torch и wall torch по-хорошему выводиться должны как torch с общим их колвом)
+    // Факела
+    if (materialList['minecraft:wall_torch']) { 
+        materialList['minecraft:torch'] = materialList['minecraft:torch'] || 0; 
+        materialList['minecraft:torch'] +=materialList['minecraft:wall_torch'];
+        delete materialList['minecraft:wall_torch'];
+    }
+    if (materialList['minecraft:redstone_wall_torch']) { 
+        materialList['minecraft:redstone_torch'] = materialList['minecraft:redstone_torch'] || 0; 
+        materialList['minecraft:redstone_torch'] +=materialList['minecraft:redstone_wall_torch'];
+        delete materialList['minecraft:redstone_wall_torch'];
+    }
+    // to-do: чекнуть как litematica сохраняет поршни (т.к.piston и piston_head это разные блоки, надо ли делить на 2 итоговое колво и т.п.)
+    // skulls в зависимости от их положения (wall или нет)
+    // Блоки, с которыми могут быть потенциально проблемы:
+    // tripwire hook (и его части)
+    // двери (их колво)
     
     // let toFind = "grindstone";
     // console.log("blocks: ", mcAssets.blocks[toFind])
@@ -88,29 +106,118 @@ export default defineEventHandler(async (event) => {
     // console.log("findItemOrBlockByName: ", mcAssets.findItemOrBlockByName(toFind))
     // console.log("getTexture: ", mcAssets.getTexture(toFind))
 
-    const blocksUsingItemImage = ['campfire', 'chain', 'lantern', 'iron_bars', 'hopper', 'lever', 'mushroom_stem', 'repeater', 'comparator', 'kelp', 'seagrass', 'oak_door', 'spruce_door', 'jungle_door', 'birch_door', 'jungle_door', 'acacia_door', 'dark_oak_door', 'mangrove_door', 'cherry_door', 'pale_oak_door', 'bamboo_door', 'crimson_door', 'warped_door', 'iron_door', 'copper_door', 'exposed_copper_door', 'weathered_copper_door', 'oxidized_copper_door', 'waxed_copper_door', 'waxed_exposed_copper_door', 'waxed_weathered_copper_door', 'waxed_oxidized_copper_door', 'candle', 'white_candle', 'light_gray_candle', 'gray_candle', 'black_candle', 'brown_candle', 'red_candle', 'orange_candle', 'yellow_candle', 'lime_candle', 'green_candle', 'cyan_candle', 'light_blue_candle', 'blue_candle', 'purple_candle', 'magenta_candle', 'pink_candle', 'pointed_dripstone', 'bell'];
+    const blocksUsingItemImage = ['campfire', 'soul_campfire', 'chain', 'lantern', 'soul_lantern', 'iron_bars', 'hopper', 'lever', 'mushroom_stem', 'repeater', 'comparator', 'kelp', 'seagrass', 'oak_door', 'spruce_door', 'jungle_door', 'birch_door', 'jungle_door', 'acacia_door', 'dark_oak_door', 'mangrove_door', 'cherry_door', 'pale_oak_door', 'bamboo_door', 'crimson_door', 'warped_door', 'iron_door', 'copper_door', 'exposed_copper_door', 'weathered_copper_door', 'oxidized_copper_door', 'waxed_copper_door', 'waxed_exposed_copper_door', 'waxed_weathered_copper_door', 'waxed_oxidized_copper_door', 'candle', 'white_candle', 'light_gray_candle', 'gray_candle', 'black_candle', 'brown_candle', 'red_candle', 'orange_candle', 'yellow_candle', 'lime_candle', 'green_candle', 'cyan_candle', 'light_blue_candle', 'blue_candle', 'purple_candle', 'magenta_candle', 'pink_candle', 'pointed_dripstone', 'bell'];
     const differentTexturesForBlocks = {
-        'bone_block': 'blocks/bone_block_side', 'sticky_piston': 'blocks/piston_top_sticky', 'composter': 'blocks/composter_side', 'crafting_table': 'blocks/crafting_table_front', 'barrel': 'blocks/barrel_side', 'bee_nest': 'blocks/bee_nest_side', 'beehive': 'blocks/beehive_front_honey', 'bookshelf': 'blocks/bookshelf', 'chiseled_bookshelf': 'blocks/chiseled_bookshelf_empty', 'crimson_hyphae': 'blocks/crimson_stem_top'
+        'bone_block': 'blocks/bone_block_side', 'sticky_piston': 'blocks/piston_top_sticky', 'composter': 'blocks/composter_side', 'crafting_table': 'blocks/crafting_table_front', 'barrel': 'blocks/barrel_side', 'bee_nest': 'blocks/bee_nest_side', 'beehive': 'blocks/beehive_front_honey', 'bookshelf': 'blocks/bookshelf', 'chiseled_bookshelf': 'blocks/chiseled_bookshelf_empty', 'crimson_hyphae': 'blocks/crimson_stem_top', 'grass_block': 'blocks/grass_block_side', 'crafter': 'blocks/crafter_top'
     };
     const blocksUsingDifferentTexture = Object.keys(differentTexturesForBlocks);
+    const handmadeTextures = {
+        'prismarine': 'prismarine_basic.png',
+        'stonecutter': 'stonecutter.webp',
+        'tall_seagrass': 'tall_seagrass.webp',
+        'sculk_shrieker': 'sculk_shrieker.webp',
+        // Beds
+        'white_bed': 'white_bed.webp',
+        'light_gray_bed': 'light_gray_bed.webp',
+        'gray_bed': 'gray_bed.webp',
+        'black_bed': 'black_bed.webp',
+        'brown_bed': 'brown_bed.webp',
+        'red_bed': 'red_bed.webp',
+        'orange_bed': 'orange_bed.webp',
+        'yellow_bed': 'yellow_bed.webp',
+        'lime_bed': 'lime_bed.webp',
+        'green_bed': 'green_bed.webp',
+        'cyan_bed': 'cyan_bed.webp',
+        'light_blue_bed': 'light_blue_bed.webp',
+        'blue_bed': 'blue_bed.webp',
+        'purple_bed': 'purple_bed.webp',
+        'magenta_bed': 'magenta_bed.webp',
+        'pink_bed': 'pink_bed.webp',
+        // Stairs
+        'end_stone_brick_stairs': 'end_stone_brick_stairs.webp',
+        'purpur_stairs': 'purpur_stairs.webp',
+        'prismarine_stairs': 'prismarine_stairs.webp',
+        'prismarine_brick_stairs': 'prismarine_brick_stairs.webp',
+        'dark_prismarine_stairs': 'dark_prismarine_stairs.webp',
+        'brick_stairs': 'brick_stairs.webp',
+        'mud_brick_stairs': 'mud_brick_stairs.webp',
+
+        'blackstone_stairs': 'blackstone_stairs.webp',
+        'polished_blackstone_stairs': 'polished_blackstone_stairs.webp',
+        'polished_blackstone_brick_stairs': 'polished_blackstone_brick_stairs.webp',
+        'cobbled_deepslate_stairs': 'cobbled_deepslate_stairs.webp',
+        'deepslate_tile_stairs': 'deepslate_tile_stairs.webp',
+        'deepslate_brick_stairs': 'deepslate_brick_stairs.webp',
+        'polished_deepslate_stairs': 'polished_deepslate_stairs.webp',
+
+        'cut_copper_stairs': 'cut_copper_stairs.webp',
+        'waxed_cut_copper_stairs': 'waxed_cut_copper_stairs.webp',
+        'exposed_cut_copper_stairs': 'exposed_cut_copper_stairs.webp',
+        'waxed_exposed_cut_copper_stairs': 'waxed_exposed_cut_copper_stairs.webp',
+        'weathered_cut_copper_stairs': 'weathered_cut_copper_stairs.webp',
+        'waxed_weathered_cut_copper_stairs': 'waxed_weathered_cut_copper_stairs.webp',
+        'oxidized_cut_copper_stairs': 'oxidized_cut_copper_stairs.webp',
+        'waxed_oxidized_cut_copper_stairs': 'oxidized_cut_copper_stairs.webp',
+
+        'oak_stairs': 'oak_stairs.webp',
+        'birch_stairs': 'birch_stairs.webp',
+        'spruce_stairs': 'spruce_stairs.webp',
+        'jungle_stairs': 'jungle_stairs.webp',
+        'dark_oak_stairs': 'dark_oak_stairs.webp',
+        'acacia_stairs': 'acacia_stairs.webp',
+        'mangrove_stairs': 'mangrove_stairs.webp',
+        'cherry_stairs': 'cherry_stairs.webp',
+        'bamboo_stairs': 'bamboo_stairs.webp',
+        'warped_stairs': 'warped_stairs.webp',
+        'crimson_stairs': 'crimson_stairs.webp',
+
+        'nether_brick_stairs': 'nether_brick_stairs.webp',
+        'red_nether_brick_stairs': 'red_nether_brick_stairs.webp',
+        'quartz_stairs': 'quartz_stairs.webp',
+        'smooth_quartz_stairs': 'smooth_quartz_stairs.webp',
+        'smooth_red_sandstone_stairs': 'smooth_red_sandstone_stairs.webp',
+        'red_sandstone_stairs': 'red_sandstone_stairs.webp',
+        'smooth_sandstone_stairs': 'smooth_sandstone_stairs.webp',
+        'sandstone_stairs': 'sandstone_stairs.webp',
+
+        'stone_stairs': 'stone_stairs.webp',
+        'stone_brick_stairs': 'stone_brick_stairs.webp',
+        'mossy_stone_brick_stairs': 'mossy_stone_brick_stairs.webp',
+        'cobblestone_stairs': 'cobblestone_stairs.webp',
+        'mossy_cobblestone_stairs': 'mossy_cobblestone_stairs.webp',
+
+        'andesite_stairs': 'andesite_stairs.webp',
+        'polished_andesite_stairs': 'polished_andesite_stairs.webp',
+        'granite_stairs': 'granite_stairs.webp',
+        'polished_granite_stairs': 'polished_granite_stairs.webp',
+        'diorite_stairs': 'diorite_stairs.webp',
+        'polished_diorite_stairs': 'polished_diorite_stairs.webp',
+    }
+    const blocksWithHandmadeTextures = Object.keys(handmadeTextures);
     // grindstone, end_rod, *_wall, *_fence, *_button, *_slab, *_stairs, *_bed, redstone_wire, sticky_piston, decorated_pot - нет текстуры, видимо, собирается из кусочков/других текстур?
     // prismarine, magma_block - нет квадратной текстуры, т.к.есть анимация
 
     // Gets texture from /./node_modules/minecraft-assets/minecraft-assets/data/1.21.1/ some item path .png
-    const getTexture = async (itemPath) => {
+    const getMcAssetsTexture = async (itemPath) => {
         const filePath = path.join(process.cwd(), '/./node_modules/minecraft-assets/minecraft-assets/data/1.21.1', itemPath + '.png');
+        const dataFile = await fs.promises.readFile(filePath);
+        return dataFile.toString('base64');
+    }
+    // gets texture from /public Nuxt folder
+    const getTexture = async (itemPath) => {
+        const filePath = path.join(process.cwd(), '/./public/mc_textures', itemPath);
         const dataFile = await fs.promises.readFile(filePath);
         return dataFile.toString('base64');
     }
     // For cases where we want block to use it's item texture
     const getItemFileForBlock = async (blockName) => {
         const itemPath = mcAssets.items[blockName]?.texture.replace("minecraft:", "").replace("block/", "blocks/");
-        return await getTexture(itemPath);
+        return await getMcAssetsTexture(itemPath);
     }
     // For cases where we want block to use other texture
     const getDifTextureFileForBlock = async (blockName) => {
         const itemPath = differentTexturesForBlocks[blockName] || "";
-        return await getTexture(itemPath);
+        return await getMcAssetsTexture(itemPath);
     }
 
     const result = [];
@@ -127,7 +234,10 @@ export default defineEventHandler(async (event) => {
         if (blocksUsingDifferentTexture.includes(shortName)) {
             texture = await getDifTextureFileForBlock(shortName);
         }
-        if (texture == null) { texture = await getTexture('misc/unknown_pack'); }
+        if (blocksWithHandmadeTextures.includes(shortName)) {
+            texture = await getTexture(handmadeTextures[shortName]);
+        }
+        if (texture == null) { texture = await getMcAssetsTexture('misc/unknown_pack'); }
 
         let newItem = {
             name: shortName.replaceAll('_', ' '),
