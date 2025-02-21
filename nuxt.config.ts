b36666741113dd159a628765e24ffe659a01290c
Andrey <alifa.stdn@gmail.com>
2025-02-21 20:35:59 +0300

    dep

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,7 +3,7 @@ export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
   modules: ['@nuxt/ui', '@nuxt/eslint'],
-  ssr: false,
+  ssr: true,
   app: {
     baseURL: '/letimaticaView/', // Убедитесь, что это соответствует вашей конфигурации
     // buildAssetsDir: '/_nuxt/' // Это значение по умолчанию, но убедитесь, что оно установлено
