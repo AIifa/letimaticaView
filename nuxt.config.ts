b46d2ca56a3b8f5cf4ddd5e253f07354c3b9128c
Andrey <alifa.stdn@gmail.com>
2025-02-21 20:20:29 +0300

    deploy

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,9 +3,9 @@ export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
   modules: ['@nuxt/ui', '@nuxt/eslint'],
-  ssr: true,
+  ssr: false,
   app: {
-    baseURL: '/letimaticaView', // Убедитесь, что это соответствует вашей конфигурации
+    baseURL: '/letimaticaView/', // Убедитесь, что это соответствует вашей конфигурации
     buildAssetsDir: '/_nuxt/' // Это значение по умолчанию, но убедитесь, что оно установлено
   },
   nitro: {
